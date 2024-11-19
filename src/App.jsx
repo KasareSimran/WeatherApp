1f9e3e6d7b71ed5f55e21e5661c87ec0d2c3322a
KasareSimran <139992213+KasareSimran@users.noreply.github.com>
2024-11-19 19:58:01 +0530

    Weather App

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import Weather from "./components/weather";
 function App(){
   return(
     <div className="app">
+      <h3 className="title">🌞Weather App🌞</h3>
         <Weather/>
     </div>
    
--- src/components/Weather.jsx
+++ src/components/Weather.jsx
@@ -1,6 +1,6 @@
 // eslint-disable-next-line no-unused-vars
-import React from "react";
-import './Weather.css'
+import React, { useEffect, useRef, useState } from "react";
+import './WeatherStyle.css'
 import search_icon from '../assets/search.svg';
 import cloudy_icon from '../assets/cloudy.png';
 import drizzle_icon from '../assets/drizzle.png';
@@ -11,32 +11,90 @@ import sun_icon from '../assets/sun.png';
 import wind_icon from '../assets/wind.png';
 function Weather(){
 
-    return(<>
+    const inputRef =useRef();
+
+    const [weatherData,setWeatherData]=useState(false)
+
+    const allIcon={
+        "01d":sun_icon,
+        "01n":sun_icon,
+        "02d":cloudy_icon,
+        "02n":cloudy_icon,
+        "03d":cloudy_icon,
+        "03n":cloudy_icon,
+        "04d":drizzle_icon,
+        "04n":drizzle_icon,
+        "09d":rain_icon,
+        "09n":rain_icon,
+        "10d":rain_icon,
+        "10n":rain_icon,
+        "13d":snow_icon,
+        "13n":snow_icon,
+    }
+    const search =async(city)=>{
+        if(city===""){
+            alert("Enter City Name");
+            return;
+        }
+        try{
+            const url=`https://api.openweathermap.org/data/2.5/weather?q=${city}&units=matric&appid=5b658fcfa0dac315ea655fda7c296848`
+
+            const response=await fetch(url);
+            const data = await response.json();
+            //if data is wrong
+            if(!response.ok){
+                alert(data.message);
+                return;
+            }
+            console.log(data);
+            const icon=allIcon[data.weather[0].icon] || sun_icon;
+            setWeatherData({
+                humidity:data.main.humidity,
+                windSpeed:data.wind.speed,
+                temperature:Math.floor(data.main.temp),
+                location:data.name,
+                icon:icon
+            })
+        // eslint-disable-next-line no-unused-vars
+        }catch (error){
+            setWeatherData(false);
+            console.error("Error in fetching weather data.")
+        }
+    }
+    useEffect(()=>{
+        search("london");
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+    },[])
+
+    return(
     <div className="main-container">
         <div className="search-bar">
-            <input type="text" placeholder="Search"/>
-            <img className='search'src={search_icon} />
+            <input  ref={inputRef} type="text" placeholder="Search"/>
+            <img className='search'src={search_icon} onClick={()=>search(inputRef.current.value)}/>
         </div>
-        <img src={sun_icon} className="weather-icon"/>
-        <p className="temperature">16°c</p>
-        <p className="location">London</p>
+
+        {/* check of data is correct then display else not  */}
+        {weatherData?<><img src={weatherData.icon} className="weather-icon"/>
+        <p className="temperature">{weatherData.temperature}°c</p>
+        <p className="location">{weatherData.location}</p>
         <div className="weather-data">
             <div className="col">
                 <img src={humidity_icon} className="info-icon"/>
                 <div>
-                    <p>91 %</p>
+                    <p>{weatherData.humidity}</p>
                     <span>Humidity</span>
                 </div>
                 </div>  
                 <div className="col">
                 <img src={wind_icon} className="info-icon"/>
                 <div>
-                    <p>3.6 km/hr</p>
+                    <p>{weatherData.windSpeed} Km/h</p>
                     <span>Wind Speed</span>
                 </div>
                 </div>   
-        </div>
+        </div></>:<></>}
+        
     </div>
-    </>);
+   );
 }
 export default Weather
